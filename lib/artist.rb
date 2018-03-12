b8478b6db7f4acb2a22873738fe28b27b86e94a7
Eddie Moon <eddiemoonie@gmail.com>
2018-03-12 20:30:19 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,4 +7,19 @@ class Artist
     @songs = []
   end
 
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def songs
+    @songs
+  end
+
+  def genres
+    self.songs.collect do |song|
+      song.genre
+    end
+  end
+
 end
